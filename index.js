a8a7f6867c9de1dfcca7e6b8dc2aae69420d1d2b
Mario Centeno <mariocenteno@guellcom.net>
2019-04-11 11:28:07 +0200

    alright  manin

--- index.js
+++ index.js
@@ -21,9 +21,57 @@ client.on("message", (message) => {
         return message.channel.send("Nos vemuo Kbsa");
     }
 
+    let condition;
+    let conditionTwo;
+    let randomName;
     if (message.content.startsWith(prefix + "changeName")) {
-        let randomName = faker.name.findName();
-        return message.member.setNickname(randomName);
+        do {
+            if (letraNombre = null) {
+                condition = 0;
+                conditionTwo = 0;
+            } else {
+                randomName = faker.name.findName();
+                let letraNombreRandom = randomName.substr(0, 1);
+                let letraNombre = message.author.username.substr(0, 1);
+                if (letraNombre == letraNombreRandom && letraNombre != null && letraNombreRandom != null) {
+                    condition = 0;
+                } else {
+                    condition = 1;
+                }
+            }
+        } while (condition != 0) {
+            if (conditionTwo == 0) {
+                return message.channel.send("No hay nombres en nuestra base de datos que empiecen por esa letra, por favor cambieselo o pongase otra letra");
+            } else {
+                return message.member.setNickname(randomName);
+            }
+        }
+
+    }
+
+    if (message.content.startsWith(prefix + "GoTName")) {
+        if (letraNombre = null) {
+            return message.channel.send("No hay nombres en nuestra base de datos que empiecen por esa letra, por favor cambieselo o pongase otra letra");
+        } else {
+            randomName = faker.name.findName();
+            let random = Math.floor(Math.random() * Math.floor(4));
+            let GoT;
+            switch (random) {
+                case 0:
+                    GoT = "Targarien";
+                    break;
+                case 1:
+                    GoT = "Lannister";
+                    break;
+                case 2:
+                    GoT = "Stark";
+                    break;
+                case 3:
+                    GoT = "Baratheon";
+                    break;
+            }
+            return message.member.setNickname(randomName.substr(0,randomName.indexOf(' ')) + " " + GoT);
+        }
     }
 
     if (message.content.startsWith(prefix)) {
